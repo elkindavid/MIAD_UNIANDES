8fd28e1c473c98ee25218dbb23f1f6de5d5b9856
elkindavid <ed.penar1@uniandes.edu.co>
2023-04-27 17:46:22 -0500

    API

---
+++ model/api.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+import joblib
+from model_deployment import predict
+
+# Definición aplicación Flask
+app = Flask(__name__)
+
+# Definición API Flask
+api = Api(
+    app, 
+    version='1.0', 
+    title='Car Price Prediction API',
+    description='Car Price Prediction API')
+
+ns = api.namespace('predict', 
+     description='Car Price Regression')
+
+# Definición argumentos o parámetros de la API
+parser = api.parser()
+# 	Year	Mileage	State	Make
+parser.add_argument(
+    'YEAR', 
+    type=int, 
+    required=True, 
+    help='Año de Fabricación', 
+    location='args')
+
+parser.add_argument(
+    'MILEAGE', 
+    type=int, 
+    required=True, 
+    help='Kilometraje', 
+    location='args')
+
+resource_fields = api.model('Resource', {
+    'result': fields.String,
+})
+
+# Definición de la clase para disponibilización
+@ns.route('/')
+class CarPriceApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        
+        return {
+         "result": predict(args['YEAR'], args['MILEAGE'])
+        }, 200
+    
+    
+if __name__ == '__main__':
+    # Ejecución de la aplicación que disponibiliza el modelo de manera local en el puerto 5000
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
---
+++ model/model_deployment.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import pandas as pd
+import joblib
+import sys
+import os
+
+def predict(year, mileage):
+    reg = joblib.load(os.path.dirname(__file__) + '/car_price_reg.pkl') 
+    car_ = pd.DataFrame([[year, mileage]], columns=['year','mileage'])
+    car_['por_make'] = 0
+    car_['por_state'] = 0
+    car_['yxm'] = (year * mileage)
+    
+#     Make prediction
+    p1 = reg.predict(car_)
+
+    return p1
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 1:
+        print('Please add an URL')
+    else:
+        
+        year = sys.argv[1]
+        mileage = sys.argv[2]
+        
+        p1 = predict(year, mileage)
+        
+        print(car)
+        print('Car Price: ', p1)
+        
